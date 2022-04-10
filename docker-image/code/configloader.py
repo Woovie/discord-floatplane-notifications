6a5af61bddbb9988e75e013fdff94fd483a32c6a
Jordan Banasik <woovie@woovie.net>
2022-04-10 11:29:21 -0700

    Update config class
    Large changes, overview:
    - Add basic method descriptions
    - Prepare env var and ini loading methods of Configuration class
    - Comment out deterministic method, for now

--- docker-image/code/configloader.py
+++ docker-image/code/configloader.py
@@ -1,16 +1,22 @@
 """
 Configuration loader for Floatplane Discord bot
+
+Supports three modes
+- Environment variables
+- config.ini
+- config.toml
 """
 import configparser
 import os
 from dataclasses import dataclass, field
-from typing import Union
 
 
 @dataclass
 class FloatplaneConfiguration:
     """
-    a
+    Floatplane configuration class
+
+    Holds 5 properties, nothing else
     """
     api_endpoint: str = "https://youshouldchangethis.com"
     video_url: str = "https://youshouldchangethis.com"
@@ -22,7 +28,8 @@ class FloatplaneConfiguration:
 @dataclass
 class DiscordConfiguration:
     """
-    a
+    Discord configuration class
+    Holds 2 properties, nothing else
     """
     channel_id: int = 0
     token: str = "youshouldchangethis"
@@ -31,9 +38,11 @@ class DiscordConfiguration:
 @dataclass
 class Configuration:
     """
-    a
+    Primary configuration class
+
+    This contains logic to figure out which configuration type is being used and preparing it for
+    usage
     """
-    config: Union[configparser.ConfigParser, dict]# TODO Rename or remove this?
     configuration_type: str = "env"
     discord: DiscordConfiguration = field(default_factory=DiscordConfiguration)
     floatplane: FloatplaneConfiguration = field(default_factory=FloatplaneConfiguration)
@@ -51,11 +60,25 @@ class Configuration:
                 self._load_toml_data()
 
     def _load_environment_data(self):
-        pass
+        env_vars = os.environ
+        self.floatplane.api_endpoint = env_vars['FP_API_ENDPOINT']
+        self.floatplane.channel = env_vars['FP_CHANNEL']
+        self.floatplane.video_url = env_vars['FP_VIDEO_URL']
+        self.floatplane.limit = int(env_vars['FP_LIMIT'])
+        self.floatplane.runner_frequency = env_vars['FP_RUNNER_FREQUENCY']
+        self.discord.channel_id = int(env_vars['DISCORD_CHANNEL_ID'])
+        self.discord.token = env_vars['DISCORD_TOKEN']
 
-    def _load_ini_data(self):# TODO Instead we should be setting this data directly to the discord and floatplane variables of the class
-        self.config = configparser.ConfigParser()
-        self.config.read('config.ini')
+    def _load_ini_data(self):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        self.floatplane.api_endpoint = config['floatplane']['api_endpoint']
+        self.floatplane.channel = config['floatplane']['channel']
+        self.floatplane.video_url = config['floatplane']['video_url']
+        self.floatplane.limit = int(config['floatplane']['limit'])
+        self.floatplane.runner_frequency = config['floatplane']['runner_frequency']
+        self.discord.channel_id = int(config['discord']['channel_id'])
+        self.discord.token = config['discord']['token']
 
     def _load_toml_data(self):
         pass
@@ -66,7 +89,7 @@ class ConfigurationLoad:
     """
     def __init__(self):
         self.config = {}
-        self._determine_configuration()
+        #self._determine_configuration()
 
     def _determine_configuration(self):
         env_vars = dict(os.environ)
